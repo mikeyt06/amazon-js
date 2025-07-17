6ef925d64c7d103e02e682de66ff95e9d7a00dd6
mikey <mikeyt030306@gmail.com>
2025-07-17 14:31:47 +0300

    generate orderes page with orders made

--- scripts/orders-page.js
+++ scripts/orders-page.js
@@ -1,4 +1,11 @@
 import { orders } from "../data/orders.js";
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+import { getProduct } from "../data/products.js";
+import { loadProducts } from '../data/products.js';
+
+loadProducts(() => {
+  renderOrdersGrid();
+});
 
 function renderOrdersGrid() {
   let orderHTML = '';
@@ -6,20 +13,29 @@ function renderOrdersGrid() {
   orders.forEach((order) => {
     let itemsHTML = '';
     order.products.forEach((orderItem) => {
+
+      const matchingProduct = getProduct(orderItem.productId);
+      if (!matchingProduct) {
+        console.warn(`Product not found for productId: ${orderItem.productId}`);
+      return; // skip this item
+}
+      const deliveryDate = dayjs(orderItem.estimatedDeliveryTime);
+      const dateString = deliveryDate.format('MMMM D');
+
       itemsHTML += `
         <div class="product-image-container">
-            <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
+            <img src="${matchingProduct.image}">
         </div>
 
           <div class="product-details">
             <div class="product-name">
-              Black and Gray Athletic Cotton Socks - 6 Pairs
+              ${matchingProduct.name}
             </div>
             <div class="product-delivery-date">
-              Arriving on: August 15
+              Arriving on: ${dateString}
             </div>
             <div class="product-quantity">
-              Quantity: 1
+              Quantity: ${orderItem.quantity}
             </div>
             <button class="buy-again-button button-primary">
               <img class="buy-again-icon" src="images/icons/buy-again.png">
@@ -36,6 +52,10 @@ function renderOrdersGrid() {
           </div>
       `;
     })
+
+    const orderDate = dayjs(order.orderTime).format('MMMM D');
+    const totalCost = `$${(order.totalCostCents / 100).toFixed(2)}`;
+    const orderId = order.id
     orderHTML += `
         <div class="order-container">
           
@@ -43,76 +63,22 @@ function renderOrdersGrid() {
             <div class="order-header-left-section">
               <div class="order-date">
                 <div class="order-header-label">Order Placed:</div>
-                <div>August 12</div>
+                <div>${orderDate}</div>
               </div>
               <div class="order-total">
                 <div class="order-header-label">Total:</div>
-                <div>$35.06</div>
+                <div>${totalCost}</div>
               </div>
             </div>
 
             <div class="order-header-right-section">
               <div class="order-header-label">Order ID:</div>
-              <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+              <div>${orderId}</div>
             </div>
           </div>
 
           <div class="order-details-grid">
-            <div class="product-image-container">
-              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 15
-              </div>
-              <div class="product-quantity">
-                Quantity: 1
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html?orderId=123&productId456">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-
-            <div class="product-image-container">
-              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 19
-              </div>
-              <div class="product-quantity">
-                Quantity: 2
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
+            ${itemsHTML}
           </div>
         </div>
     `;
@@ -122,89 +88,5 @@ function renderOrdersGrid() {
 
 }
 
-renderOrdersGrid();
 
 
-
-
-
-
-/*
-        <div class="order-container">
-          
-          <div class="order-header">
-            <div class="order-header-left-section">
-              <div class="order-date">
-                <div class="order-header-label">Order Placed:</div>
-                <div>August 12</div>
-              </div>
-              <div class="order-total">
-                <div class="order-header-label">Total:</div>
-                <div>$35.06</div>
-              </div>
-            </div>
-
-            <div class="order-header-right-section">
-              <div class="order-header-label">Order ID:</div>
-              <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
-            </div>
-          </div>
-
-          <div class="order-details-grid">
-            <div class="product-image-container">
-              <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 15
-              </div>
-              <div class="product-quantity">
-                Quantity: 1
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html?orderId=123&productId456">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-
-            <div class="product-image-container">
-              <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-            </div>
-
-            <div class="product-details">
-              <div class="product-name">
-                Adults Plain Cotton T-Shirt - 2 Pack
-              </div>
-              <div class="product-delivery-date">
-                Arriving on: August 19
-              </div>
-              <div class="product-quantity">
-                Quantity: 2
-              </div>
-              <button class="buy-again-button button-primary">
-                <img class="buy-again-icon" src="images/icons/buy-again.png">
-                <span class="buy-again-message">Buy it again</span>
-              </button>
-            </div>
-
-            <div class="product-actions">
-              <a href="tracking.html">
-                <button class="track-package-button button-secondary">
-                  Track package
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>*/
